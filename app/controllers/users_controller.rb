73f020424029f20d75104af10b5bfb67c40f8a65
abdellani <abdellani@gmail.com>
2019-08-22 18:20:23 +0100

    Add edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,17 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
+  def edit 
+    @user= User.find_by(id:params[:id])
+  end
+  def update
+    @user= User.find_by(id:params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to new_user_path
+    else 
+      render :edit
+    end
+  end
   private
     def user_params
       params[:user].permit(:name,:email,:password) 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
 
 end
