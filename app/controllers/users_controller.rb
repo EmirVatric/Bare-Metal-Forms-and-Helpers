22764f2bd0e7f28dc0d9eb6082044eada2db3a34
EmirVatric <vatricemir@gmail.com>
2019-08-26 17:12:07 +0200

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,10 @@ class UsersController < ApplicationController
     end
   end
   def edit 
-    @user= User.find_by(id:params[:id])
+    @user= User.find(params[:id])
   end
   def update
-    @user= User.find_by(id:params[:id])
+    @user= User.find(params[:id])
     if @user.update_attributes(user_params)
       redirect_to new_user_path
     else 
